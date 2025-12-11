dc9d857203325eba19b4c7f189e17d076d495019
Tanveer1907 <1906tanveer@gmail.com>
2025-12-11 19:59:30 +0530

    .

--- Java/strings/compress_string.java
+++ Java/strings/compress_string.java
@@ -1,7 +1,19 @@
 package strings;
+import java.util.*;
 public class compress_string {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+        for(int i = 0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int count =1;
+            while(i+1<str.length() && ch == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            result.append(ch).append(count);
+        }
+        System.out.print(result);
     }
     
-}
