d725f7dfafcfc7c98754c16b631674bd5f0a0934
Tanveer1907 <1906tanveer@gmail.com>
2025-12-13 20:39:08 +0530

    practice

---
+++ Java/st3_practice/memory_lane.java
@@ -0,0 +1,47 @@
+package st3_practice;
+public class memory_lane {
+    public static class Node {
+        public int data;
+        public Node next;
+
+        Node(int data1, Node next1){
+            data = data1;
+            next = next1;
+        }
+        public Node(int data1){
+            data = data1;
+            next = null;
+        }
+
+        public static Node convertArrtoLL(int[] arr){
+            Node head = new Node(arr[0]);
+            Node mover = head;
+            for(int i = 1; i < arr.length; i++){
+                Node temp = new Node(arr[i]);
+                mover.next = temp;
+                mover = temp;
+            }
+            return head;
+        }
+        public static Node reverse(Node head){
+            Node curr = head;
+            Node prev = null;
+            while(curr!=null){
+                Node newnode = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = newnode;
+            }
+            return prev;
+        }
+    }
+    public static void main(String[] args) {
+         int[] arr = {1,2,3,4,5};
+        Node head = Node.convertArrtoLL(arr);
+        Node ans = Node.reverse(head);
+        while(ans!=null){
+            System.out.print(ans.data+" ");
+            ans = ans.next;
+        }
+    }
+}
