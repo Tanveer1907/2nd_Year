bf656ab67665be4a224a651620a84054cd345d0c
Tanveer1907 <1906tanveer@gmail.com>
2025-12-13 20:38:41 +0530

    .

--- Java/strings/compress_string.java
+++ Java/strings/compress_string.java
@@ -1,19 +1,22 @@
 package strings;
+
 import java.util.*;
 public class compress_string {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        StringBuilder result = new StringBuilder();
-        for(int i = 0;i<str.length();i++){
-            char ch = str.charAt(i);
-            int count =1;
-            while(i+1<str.length() && ch == str.charAt(i+1)){
-                count++;
-                i++;
+        try (Scanner sc = new Scanner(System.in)) {
+            String str = sc.nextLine();
+            StringBuilder result = new StringBuilder();
+            for(int i = 0;i<str.length();i++){
+                char ch = str.charAt(i);
+                int count =1;
+                while(i+1<str.length() && ch == str.charAt(i+1)){
+                    count++;
+                    i++;
+                }
+                result.append(ch).append(count);
             }
-            result.append(ch).append(count);
+            System.out.print(result);
         }
-        System.out.print(result);
     }
     
+}
